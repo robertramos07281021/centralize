c127e76cc7f0e7f98e9d8479d5d55183e2cac8a3
Rained <edrianloberiano019@gmail.com>
2025-10-04 14:51:01 +0800

    ..

--- client/src/pages/agent/MixedChartView.tsx
+++ client/src/pages/agent/MixedChartView.tsx
@@ -1,23 +1,22 @@
-import { color, date, month } from "../../middleware/exports"
-import { ChartData, ChartDataset, ChartOptions, Plugin } from "chart.js"
-import gql from 'graphql-tag';
-import { useQuery } from '@apollo/client';
-import { useEffect, useMemo } from 'react';
+import { color, date, month } from "../../middleware/exports";
+import { ChartData, ChartDataset, ChartOptions, Plugin } from "chart.js";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { useEffect, useMemo } from "react";
 import { Chart } from "react-chartjs-2";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useLocation } from "react-router-dom";
 
-
 type AgentProdPerDay = {
-  total: number
-  date: number
-  ptp: number
-  ptp_kept: number
-  paid: number
-  variance: number
-}
+  total: number;
+  date: number;
+  ptp: number;
+  ptp_kept: number;
+  paid: number;
+  variance: number;
+};
 
 const AGENT_PER_DAY_PROD = gql`
   query getAgentProductionPerDay {
@@ -29,22 +28,29 @@ const AGENT_PER_DAY_PROD = gql`
       paid
     }
   }
-`
+`;
 
 const MixedChartView = () => {
-  const location = useLocation()
-  const isAgentDashboard = location.pathname.includes('agent-dashboard')
-  const {data:agentProdPerDayData, refetch:PerDayRefetch, loading} = useQuery<{getAgentProductionPerDay:AgentProdPerDay[]}>(AGENT_PER_DAY_PROD,{notifyOnNetworkStatusChange: true, skip: !isAgentDashboard})
-  const {userLogged} = useSelector((state:RootState)=> state.auth)
+  const location = useLocation();
+  const isAgentDashboard = location.pathname.includes("agent-dashboard");
+  const {
+    data: agentProdPerDayData,
+    refetch: PerDayRefetch,
+    loading,
+  } = useQuery<{ getAgentProductionPerDay: AgentProdPerDay[] }>(
+    AGENT_PER_DAY_PROD,
+    { notifyOnNetworkStatusChange: true, skip: !isAgentDashboard }
+  );
+  const { userLogged } = useSelector((state: RootState) => state.auth);
 
-  useEffect(()=> {
-    const refetching = async()=> {
-      await PerDayRefetch()
-    }
-    refetching()
-  },[])
+  useEffect(() => {
+    const refetching = async () => {
+      await PerDayRefetch();
+    };
+    refetching();
+  }, []);
 
-  const dailyTargets = userLogged?.targets?.daily || 0
+  const dailyTargets = userLogged?.targets?.daily || 0;
 
   function getDaysInMonth(): number {
     const currentMonth = new Date().getMonth();
@@ -55,28 +61,28 @@ const MixedChartView = () => {
     return Array.from({ length: getDaysInMonth() }, (_, i) => i + 1);
   }
 
-  const newProdData = useMemo(()=> {
-    return agentProdPerDayData?.getAgentProductionPerDay.map(x=> {
-      const theVariance = dailyTargets - (x.ptp_kept)
+  const newProdData = useMemo(() => {
+    return agentProdPerDayData?.getAgentProductionPerDay.map((x) => {
+      const theVariance = dailyTargets - x.ptp_kept;
       return {
         ...x,
-        variance: theVariance
-      }
-    })
-  },[agentProdPerDayData, dailyTargets])
+        variance: theVariance,
+      };
+    });
+  }, [agentProdPerDayData, dailyTargets]);
 
   const daysInMonth = getDaysInMonth();
   const prodData = newProdData || [];
-  const totals = prodData.map(e=> e.total)
-  const rounded = Math.round(Math.max(...totals)/10000)*10000
-  const Max = rounded > dailyTargets ? rounded : dailyTargets
-  
+  const totals = prodData.map((e) => e.total);
+  const rounded = Math.round(Math.max(...totals) / 10000) * 10000;
+  const Max = rounded > dailyTargets ? rounded : dailyTargets;
+
   const fields: {
     label: string;
     key: keyof AgentProdPerDay;
     color: string;
     type: "bar" | "line";
-  }[]= [
+  }[] = [
     { label: "PTP", key: "ptp", color: color[2], type: "bar" },
     { label: "Kept", key: "ptp_kept", color: color[7], type: "bar" },
     // { label: "Paid Collected", key: "paid", color: color[15], type: "bar" },
@@ -84,36 +90,43 @@ const MixedChartView = () => {
     { label: "Variance", key: "variance", color: "red", type: "line" },
   ];
 
-  const datasets:ChartDataset<'bar' | 'line', number[]>[] = fields.map(({ label, key, color, type }) => ({
-    type,
-    label,
-    data: Array.from({ length: daysInMonth }, (_, i) => {
-      const entry:{[key:string]:number} = prodData.find(e => e.date - 1 === i) || {};
+  const datasets: ChartDataset<"bar" | "line", number[]>[] = fields.map(
+    ({ label, key, color, type }) => ({
+      type,
+      label,
+      data: Array.from({ length: daysInMonth }, (_, i) => {
+        const entry: { [key: string]: number } =
+          prodData.find((e) => e.date - 1 === i) || {};
 
-      return entry[key] || 0;
-    }),
-    ...(type === "line"
-      ? {
-          borderColor: color,
-          borderWidth: 0.8,
-          fill: false,
-          tension: 0.4,
-          yAxisID: "y1",
-        }
-      : {
-          backgroundColor: color,
-        }),
-  }));
+        return entry[key] || 0;
+      }),
+      ...(type === "line"
+        ? {
+            borderColor: color,
+            borderWidth: 0.8,
+            fill: false,
+            tension: 0.4,
+            yAxisID: "y1",
+          }
+        : {
+            backgroundColor: color,
+          }),
+    })
+  );
 
-  const dataPerDay: ChartData<"bar" | 'line', number[], unknown> = {
+  const dataPerDay: ChartData<"bar" | "line", number[], unknown> = {
     labels: getDayLabels(),
     datasets,
   };
 
-  const customGridLinePlugi:Plugin<'bar' | 'line'> = {
-    id: 'customGridLine',
+  const customGridLinePlugi: Plugin<"bar" | "line"> = {
+    id: "customGridLine",
     beforeDraw: (chart) => {
-      const { ctx, chartArea: { left, right }, scales: { y } } = chart;
+      const {
+        ctx,
+        chartArea: { left, right },
+        scales: { y },
+      } = chart;
       const yValue = dailyTargets;
       const yPixel = y.getPixelForValue(yValue);
       ctx.save();
@@ -121,36 +134,35 @@ const MixedChartView = () => {
       ctx.moveTo(left, yPixel);
       ctx.lineTo(right, yPixel);
       ctx.lineWidth = 1.5;
-      ctx.strokeStyle = 'red';
+      ctx.strokeStyle = "red";
       ctx.stroke();
 
-      ctx.fillStyle = 'red';
-      ctx.font = '12px sans-serif';
+      ctx.fillStyle = "red";
+      ctx.font = "12px sans-serif";
       ctx.fillText(``, left + 4, yPixel - 4);
       ctx.restore();
-    }
+    },
   };
 
-
-  const optionPerDay:ChartOptions<'bar'| 'line'> = { 
+  const optionPerDay: ChartOptions<"bar" | "line"> = {
     plugins: {
-      datalabels:{
-        display:false
+      datalabels: {
+        display: false,
       },
       legend: {
         display: true,
         labels: {
           font: {
             size: 14,
-            family: 'Arial',
-            weight: 'bold',
+            family: "Arial",
+            weight: "bold",
           },
         },
       },
       title: {
         display: true,
         text: `Month of ${month[new Date().getMonth()]}`,
-      },     
+      },
     },
     scales: {
       x: {
@@ -161,8 +173,8 @@ const MixedChartView = () => {
         },
       },
       y: {
-        type: 'linear',
-        position: 'left',
+        type: "linear",
+        position: "left",
         min: 0,
         max: Max + 20000,
         ticks: {
@@ -172,8 +184,8 @@ const MixedChartView = () => {
         },
       },
       y1: {
-        type: 'linear',
-        position: 'right',
+        type: "linear",
+        position: "right",
         min: 0,
         max: Max + 20000,
         ticks: {
@@ -183,29 +195,28 @@ const MixedChartView = () => {
         },
       },
     },
-    responsive: true, 
-    maintainAspectRatio: false
-  }
+    responsive: true,
+    maintainAspectRatio: false,
+  };
 
   return (
     <div className="border flex rounded-lg border-slate-200 lg:col-span-6 lg:row-span-2 p-2 shadow-md shadow-black/20 bg-white">
-      {
-        loading ? 
+      {loading ? (
         <div className="flex justify-center items-center w-full">
           <AiOutlineLoading3Quarters className="animate-spin text-7xl" />
-
         </div>
-        :
-
-        <Chart type="bar" data={dataPerDay} options={optionPerDay} plugins={[customGridLinePlugi]}  />
-      }
-
+      ) : (
       
+        <Chart
+          type="bar"
+          data={dataPerDay}
+          options={optionPerDay}
+          plugins={[customGridLinePlugi]}
+        />
+        
+      )}
     </div>
-  
-  )
-}
+  );
+};
 
-export default MixedChartView
-
-
+export default MixedChartView;
--- client/src/pages/agent/StatisticsView.tsx
+++ client/src/pages/agent/StatisticsView.tsx
@@ -1,24 +1,81 @@
-import AgentTimer from "./AgentTimer"
-import MixedChartView from "./MixedChartView"
-import MixedChartMonthView from "./MixedChartMonthView"
-import OverallPerformance from "./OverallPerformance"
-import DashboardMinis from "./DashboardMinis.tsx"
+import AgentTimer from "./AgentTimer";
+import MixedChartView from "./MixedChartView";
+import MixedChartMonthView from "./MixedChartMonthView";
+import OverallPerformance from "./OverallPerformance";
+import DashboardMinis from "./DashboardMinis.tsx";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store.ts";
+import { CgDanger } from "react-icons/cg";
+import { useState } from "react";
+import { AnimatePresence, motion, scale } from "framer-motion";
 
 const StatisticsView = () => {
+  const { userLogged } = useSelector((state: RootState) => state.auth);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex flex-col lg:h-full bg-slate-200 lg:overflow-hidden">
+    <div className="flex flex-col lg:h-full bg-slate-200 relative lg:overflow-hidden ">
+      {
+        !userLogged?.vici_id &&
+        <CgDanger
+          onClick={() => setOpen(true)}
+          className={`" absolute top-12 right-4 w-6 h-6 p-[2px] shadow-md cursor-pointer animate-pulse rounded-full  bg-red-800 "`}
+          color="white"
+        />
+      }
       <div className="p-2">
-        <AgentTimer/>
+        <AgentTimer />
       </div>
       <div className=" h-full w-full grid grid-cols-1 lg:grid-cols-9 lg:grid-rows-4 lg:gap-2 overflow-hidden grid-rows-3 gap-2 p-2">
-        <DashboardMinis/>
-        <MixedChartView/>
-        <MixedChartMonthView/>
-        <OverallPerformance/>
+        <DashboardMinis />
+        <MixedChartView />
+        <MixedChartMonthView />
+        <OverallPerformance />
       </div>
-    </div>
-  )
-}
+      <AnimatePresence>
+        {open && (
+          <div className="absolute top-0 justify-center z-50 items-center flex left-0 w-full h-full">
+            <motion.div
+              onClick={() => setOpen(false)}
+              className="bg-[#00000073] cursor-pointer h-full w-full backdrop-blur-xs"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            ></motion.div>
+            <div className="absolute">
+              <motion.div
+                className="bg-white   p-5 rounded-md shadow-md"
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{scale: 0.5, opacity: 0}}
+                transition={{}}
+              >
+                <div className="mb-3 text-center font-black uppercase text-3xl">
+                  input the vcI id
+                </div>
+                <input
+                  className="bg-gray-200 w-full px-4 py-1 rounded-md shadow-md"
+                  placeholder="ha?"
+                />
 
-export default StatisticsView
+                <div className="flex mt-5 font-black text-white  uppercase gap-3">
+                  <div
+                    onClick={() => setOpen(false)}
+                    className="py-2 px-3 bg-red-600 border-2 border-red-800 hover:bg-red-800 cursor-pointer transition-all rounded-md shadow-md "
+                  >
+                    No
+                  </div>
+                  <div className="py-2 w-full text-center border-2 border-green-800 hover:bg-green-800 transition-all cursor-pointer bg-green-600 rounded-md shadow0-md">
+                    yes
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          </div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default StatisticsView;
