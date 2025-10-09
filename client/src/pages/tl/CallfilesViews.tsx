454c14f0daf1e20243d202d6695cc6f0948ccf97
robertramos07281021 <robert.ramos07281021@gmail.com>
2025-10-09 15:24:56 +0800

    3:01

--- client/src/pages/tl/CallfilesViews.tsx
+++ client/src/pages/tl/CallfilesViews.tsx
@@ -652,7 +652,7 @@ const CallfilesViews: React.FC<Props> = ({
               return (
                 <div
                   key={index}
-                  className="text-[0.7rem] py-2 px-3 bg-gray-100 even:bg-gray-200 2xl:text-xs gap-2 text-gray-600 grid grid-cols-[repeat(15,_minmax(0,_1fr))] w-full "
+                  className="text-[0.7rem] items-center py-2 px-3 bg-gray-100 even:bg-gray-200 2xl:text-xs gap-2 text-gray-600 grid grid-cols-[repeat(15,_minmax(0,_1fr))] w-full "
                 >
                   <div className="truncate pr-2" title={res.callfile.name}>
                     {res.callfile.name}
@@ -670,25 +670,36 @@ const CallfilesViews: React.FC<Props> = ({
                   <div>{res.accounts}</div>
                   <div>{res.uncontactable || 0}</div>
                   <div>{res.connected}</div>
-                  <div className="truncate">
+                  <div
+                    className="truncate cursor-default"
+                    title={res.OB.toLocaleString("en-PH", {
+                      style: "currency",
+                      currency: "PHP",
+                    })}
+                  >
                     {res.OB.toLocaleString("en-PH", {
                       style: "currency",
                       currency: "PHP",
                     })}
                   </div>
-                  <div className="truncate">
+                  <div className="truncate cursor-default"
+                  title={res.principal.toLocaleString("en-PH", {
+                      style: "currency",
+                      currency: "PHP",
+                    })}
+                  >
                     {res.principal.toLocaleString("en-PH", {
                       style: "currency",
                       currency: "PHP",
                     })}
                   </div>
-                  <div className="truncate">
+                  <div className="truncate cursor-default">
                     {res.target.toLocaleString("en-PH", {
                       style: "currency",
                       currency: "PHP",
                     })}
                   </div>
-                  <div className="truncate">
+                  <div className="truncate cursor-default">
                     {res.collected.toLocaleString("en-PH", {
                       style: "currency",
                       currency: "PHP",
@@ -700,7 +711,7 @@ const CallfilesViews: React.FC<Props> = ({
                     {checkStatus && (
                       <>
                         <div
-                          className="rounded-sm flex justify-center hover:bg-purple-800 transition-all px-1 py-1 cursor-pointer bg-purple-700 text-white border border-purple-800"
+                          className="rounded-sm shadow-sm flex justify-center hover:bg-purple-800 transition-all px-1 py-1 cursor-pointer bg-purple-700 text-white border border-purple-800"
                           onClick={() => {
                             setAddSelectiveModal((prev) => !prev);
                             setCallfile(res.callfile._id);
@@ -710,7 +721,7 @@ const CallfilesViews: React.FC<Props> = ({
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
-                            stroke-width="2"
+                            stroke-width="3"
                             stroke="currentColor"
                             className="size-5 "
                           >
@@ -723,7 +734,7 @@ const CallfilesViews: React.FC<Props> = ({
                         </div>
 
                         <div
-                          className="rounded-sm flex justify-center hover:bg-green-800 px-1 py-1 cursor-pointer bg-green-700 text-white border border-green-800"
+                          className="rounded-sm flex shadow-sm justify-center hover:bg-green-800 px-1 py-1 cursor-pointer bg-green-700 text-white border border-green-800"
                           onClick={() =>
                             onClickIcon(
                               res.callfile._id,
@@ -747,22 +758,37 @@ const CallfilesViews: React.FC<Props> = ({
                             />
                           </svg>
                         </div>
-
-                        <FaSquareCheck className="hover:scale-110 text-green-500 lg:text-xs 2xl:text-lg cursor-pointer" />
-
-                        <IoSettingsSharp
-                          className="text-orange-500 lg:text-xs 2xl:text-lg cursor-pointer hover:scale-110"
-                          title="Set Target"
+                        <div
                           onClick={() => {
                             setModalTarget(true);
                             setCallfileId(res.callfile);
                           }}
-                        />
+                          className="rounded-sm flex shadow-sm justify-center hover:bg-orange-800 px-1 py-1 cursor-pointer bg-orange-700 text-white border border-orange-800"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            className="size-5"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+                            />
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                          </svg>
+                        </div>
                       </>
                     )}
                     {/* <FaTrash className=" text-red-500 lg:text-xs 2xl:text-lg cursor-pointer hover:scale-110" onClick={()=> onClickIcon(res.callfile._id, "DELETE", res.callfile.name)} title="Delete"/> */}
-                    <FaDownload
-                      className="text-blue-500 lg:text-xs 2xl:text-lg cursor-pointer hover:scale-110"
+                    <div
                       onClick={() =>
                         onClickIcon(
                           res.callfile._id,
@@ -770,8 +796,23 @@ const CallfilesViews: React.FC<Props> = ({
                           res.callfile.name
                         )
                       }
-                      title="Download"
-                    />
+                      className="rounded-sm flex shadow-sm justify-center hover:bg-blue-800 px-1 py-1 cursor-pointer bg-blue-700 text-white border border-blue-800"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        className="size-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
               );
